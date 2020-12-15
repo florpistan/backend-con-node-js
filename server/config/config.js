09ca07e4f8e3786930cf33799a37dd4ee66fc4f9
Florencia Pistán Esparza <flor.pistan.esparza@gmail.com>
2020-12-14 21:35:55 -0300

    put, get, delete

--- server/config/config.js
+++ server/config/config.js
@@ -1,4 +1,19 @@
 
 //*******Puerto******
 //devuelve el puerto de nuestro proyecto si está en un servidor externo, cuando no está en entorno de producción toma el puerto local 3005 (en este caso)
-process.env.PORT=process.env.PORT || 3005
+process.env.PORT = process.env.PORT || 3005
+
+//========Definir el entorno======
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+//===Base de Datos===
+let urlDB;
+
+if(process.env.NODE_ENV === 'dev'){
+    urlDB = 'mongodb://localhost:27017/rolling'
+} else {
+    urlDB = 'mongodb+srv://user:rollingcode2020@cluster0.ccoma.mongodb.net/rolling'
+}
+
+process.env.urlDB = urlDB;
--- server/rutas/usuario.js
+++ server/rutas/usuario.js
@@ -1,6 +1,11 @@
 const express = require("express");
 //encriptar contraseñas
 const bcrypt = require('bcrypt');
+//-------------
+
+// importando underscore para validaciones del put
+const _ = require('underscore');
+// -------------------
 
 const app = express();
 
@@ -10,10 +15,46 @@ const Usuario = require('../models/usuario')
 
 
 app.get("/usuario", function (req, res) {
-    //req (solicitud) res (respuesta)
-    res.json({
-        message: "GET usuario",
-    });
+    let desde = req.query.desde || 0;
+    desde = Number(desde)
+
+    let limite = req.query.limite || 5;
+    limite = Number(limite)
+
+    Usuario.find({estado:true})
+
+        .skip(desde) //muestra a partir del registro nro 5
+
+        .limit(limite) //limita la cantidad de registros en pantalla    
+        .exec((err, usuarios) => {
+            if (err) {
+                return res.status(400).json({
+                    ok: false,
+                    err
+                });
+            }
+
+            Usuario.count({estado: true}, (err, conteo) => {
+                if (err) {
+                    return res.status(400).json({
+                        ok: false,
+                        err
+                    });
+                }
+
+                res.json({
+                    ok: true,
+                    usuarios,
+                    cantidad: conteo,
+                });
+
+            })
+
+
+
+        })
+
+
 });
 
 //MÉTODO POST
@@ -55,17 +96,57 @@ app.post("/usuario", function (req, res) {
     //     });
     // }
 });
-app.put("/usuario", function (req, res) {
+app.put("/usuario/:id", function (req, res) {
+    let id = req.params.id;
+
+    let body = _.pick(req.body, ['nombre', 'email', 'img', 'role', 'estado']);
+
+    Usuario.findByIdAndUpdate(id, body, { new: true, runValidators: true }, (err, usuarioDB) => {
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                err
+            });
+        }
+        res.json({
+            ok: true,
+            usuario: usuarioDB,
+        });
+    })
     //req (solicitud) res (respuesta)
-    res.json({
-        message: "PUT usuario",
-    });
+    // res.json({
+    //     message: "PUT usuario",
+    // });
 });
-app.delete("/usuario", function (req, res) {
+app.delete("/usuario/:id", function (req, res) {
     //req (solicitud) res (respuesta)
-    res.json({
-        message: "DELETE usuario",
-    });
+    let id = req.params.id;
+
+    let estadoActualizado = {
+        estado: false,
+    }
+
+    Usuario.findByIdAndUpdate(id, estadoActualizado, { new: true }, (err, usuarioBorrado) => {
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                err
+            });
+        }
+
+        if (!usuarioBorrado) {
+            return res.status(400).json({
+                ok: false,
+                message: 'Usuario no encontrado',
+            });
+        }
+
+        res.json({
+            ok: true,
+            usuario: usuarioBorrado
+        });
+    })
+
 });
 
 module.exports = app;
